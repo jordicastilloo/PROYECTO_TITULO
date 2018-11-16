8cfc2b243166a1e97e03960d2bc7122e12e10eea
Nicolas Llanos <nicolas.llanosm@gmail.com>
2018-11-16 10:52:14 -0300

    personal

--- app/Http/Controllers/PersonalController.php
+++ app/Http/Controllers/PersonalController.php
@@ -26,7 +26,7 @@ class PersonalController extends Controller {
 	public function create()
 	{
 		//
-		return view('personals.createUpdate');
+		return view('admin.personal.createUpdate');
 	}
 
 	/**
--- app/Personal.php
+++ app/Personal.php
@@ -6,7 +6,7 @@ class Personal extends Model {
 
 	//
     public $timestamps = false;
-    protected $table = 'personal';
+    protected $table = 'personals';
 	protected $primaryKey = 'rut_p';
 	//protected $
 	protected $fillable = ['rut_p','tipo_p', 'nombre_p', 'ap_pat_p','ap_mat_p','telefono_p','email_p','contrasena_p','nacionalidad_p','fecha_nac','sexo_p','profesion_p','fotografia_p','empresa_id_emp'];
