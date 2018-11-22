839eb49a9b1435e495bf11abc1424f2baee23ae5
Nicolas Llanos <nicolas.llanosm@gmail.com>
2018-11-21 23:27:40 -0300

    estilo boton modal

--- resources/views/admin/personal/botonmodal.blade.php
+++ resources/views/admin/personal/botonmodal.blade.php
@@ -1,31 +1,41 @@
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 
-<div class="modal fade" tabindex="-1" id="modal-delete-{{$Persona->rut_p}}" role="dialog">
+
+
+<div class="modal fade" tabindex="-1" id="modal-delete-{{$Persona->rut_p}}" role="dialog" >
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
+
+            
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
                             aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title">Datos de la Persona</h4>
             </div>
             <div class="modal-body">
 
+
+
+
+
             	<ul>
-            		<br>Rut de la Persona: {{$Persona->rut_p}}
-                    <br>Rut de la Persona: {{$Persona->tipo_p}}
-            		<br>Nombre de la Persona: {{$Persona->nombre_p}}
-                    <br>Rut de la Persona: {{$Persona->ap_pat_p}}
-                    <br>Rut de la Persona: {{$Persona->ap_mat_p}}
-                    <br>Rut de la Persona: {{$Persona->telefono_p}}
-                    <br>Rut de la Persona: {{$Persona->email_p}}
-                    <br>Rut de la Persona: {{$Persona->contrasena_p}}
-                    <br>Rut de la Persona: {{$Persona->nacionalidad_p}}
-                    <br>Rut de la Persona: {{$Persona->fecha_nac_p}}
-                    <br>Rut de la Persona: {{$Persona->sexo_p}}
-                    <br>Rut de la Persona: {{$Persona->profesion_p}}
-                    <br>Rut de la Persona: {{$Persona->fotografia_p}}
+            		<br><h4>Rut: {{$Persona->rut_p}}
+                    <br>Cargo: {{$Persona->tipo_p}}
+            		<br>Nombre: {{$Persona->nombre_p}}
+                    <br>Apellido Paterno: {{$Persona->ap_pat_p}}
+                    <br>Apellido Materno: {{$Persona->ap_mat_p}}
+                    <br>Telefono: {{$Persona->telefono_p}}
+                    <br>Email: {{$Persona->email_p}}
+                    <br>Contraseña: {{$Persona->contrasena_p}}
+                    <br>Nacionalidad: {{$Persona->nacionalidad_p}}
+                    <br>Fecha de Nacimiento: {{$Persona->fecha_nac_p}}
+                    <br>Genero: {{$Persona->sexo_p}}
+                    <br>Profesion: {{$Persona->profesion_p}}
+                    <br>Fotografia: {{$Persona->fotografia_p}}
             	</ul>
-                
+
             </div>
             <div class="modal-footer">
                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
