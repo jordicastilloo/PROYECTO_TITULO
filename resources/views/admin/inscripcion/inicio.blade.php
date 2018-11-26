109e86e809f2e72ad4579044a355d0bba1f24ecc
Jordi Castillo <datos.jordi@gmail.com>
2018-11-26 11:03:03 -0300

    a

--- resources/views/admin/inscripcion/inicio.blade.php
+++ resources/views/admin/inscripcion/inicio.blade.php
@@ -3,15 +3,23 @@
 @section('content')
 <div class="panel panel-primary" width="30px">
 	<div class="panel-heading">Título del panel</div>
-	<div class="panel-body"></div>
+	<div class="panel-body">
+		Plan 1
+		Plan 2
+		Plan 3
+	</div>
 </div>
 <div class="panel panel-primary">
 	<div class="panel-heading">Título del panel</div>
-	<div class="panel-body"></div>
+	<div class="panel-body">
+		Datos cliente
+	</div>
 </div>
 <div class="panel panel-primary">
 	<div class="panel-heading">Título del panel</div>
-	<div class="panel-body"></div>
+	<div class="panel-body">
+		confirmar
+	</div>
 </div>
 
 @endsection
