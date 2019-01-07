2bf556e081848a3664eb60c4abb9e7414a77e7e0
Jordi Castillo <datos.jordi@gmail.com>
2019-01-07 17:12:38 -0300

    plan_n

--- app/Plan_Nutricional.php
+++ app/Plan_Nutricional.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 class Plan_Nutricional extends Model {
 
 	public $timestamps = false;
-	protected $table = 'plan_nutricional';
+	protected $table = 'plan__nutricionals';
 	protected $primaryKey = 'id_plan_nut';
 	protected $fillable = ['nombre_plan_nut', 'descripcion_plan_nut', 'rut_p','rut_cliente'];
 	protected $guarded = ['id_plan_nut'];
